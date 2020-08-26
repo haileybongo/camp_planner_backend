8d6eb4678a9400694df2659586fc616b7aacd130
haileybongo <haileyebongo@gmail.com>
2020-08-26 16:29:31 -0700

    updating associations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
     end
 
     def auth_header
-      binding.pry
+
         request.headers['Authorization']
 
     end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
 class Trip < ApplicationRecord
     belongs_to :user, optional: true
-    has_one :item
+    has_one :item, dependent: :destroy
 end
